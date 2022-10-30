f58c6fa1ca934d29272e5892f1e625d75661d721
walgleisson valerio <walgleissonvaleriodev@gmail.com>
2022-10-29 23:21:18 -0300

    Cria os modelos de category e postCategory

---
+++ src/models/PostCategory.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: { type: DataTypes.INTEGER, primaryKey: true },
+    categoryId: { type: DataTypes.INTEGER, primaryKey: true },
+  }, {
+    tableName: 'posts_categories',
+    timestamps: false,
+    underscored: true,
+  });
+
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'posts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherId: 'postId',
+    });
+
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherId: 'categoryId',
+    });
+  }
+
+  return PostCategory;
+};
